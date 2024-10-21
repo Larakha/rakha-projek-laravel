232d76c76df2f0f13c4414b9f9bb45a39446c364
Larakha <Larakhaaap@gmail.com>
2024-10-21 15:55:46 +0700

    tailwind commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,39 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perkenalan Diri</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    @vite('resources/css/app.css')
 </head>
 <body>
-    <header>
-        <h1>Selamat Datang!</h1>
-        <p>Perkenalan Diri - Larakha Hasta Agung Prayoga</p>
-    </header>
+<div class="bg-green-500 text-white p-8">
+  <h1 class="text-2xl font-bold">Selamat Datang!</h1>
+  <p class="mt-4">Perkenalan Diri - Larakha Hasta Agung Prayoga</p>
 
-    <section id="about">
-        <h2>Tentang Saya</h2>
-        <p>Halo, nama saya Larakha Hasta Agung Prayoga. Saya seorang pelajar SMK Telkom Purwokerto.</p>
-    </section>
+  <div class="mt-8">
+    <h2 class="text-lg font-bold">Tentang Saya</h2>
+    <p class="mt-2">Halo, nama saya Larakha Hasta Agung Prayoga. Saya seorang pelajar SMK Telkom Purwokerto.</p>
+  </div>
 
-    <section id="skills">
-        <h2>Keterampilan</h2>
-        <ul>
-            <li>Design Grafis</li>
-            <li>Fotografi</li>
-        </ul>
-    </section>
+  <div class="mt-8">
+    <h2 class="text-lg font-bold">Keterampilan</h2>
+    <ul class="list-disc pl-6 mt-2">
+      <li>Design Grafis</li>
+      <li>Fotografi</li>
+    </ul>
+  </div>
 
-    <section id="contact">
-        <h2>Kontak</h2>
-        <p>Anda bisa menghubungi saya melalui:</p>
-        <ul>
-            <li>Email: <a href="Larakhaaap@gmail.com">Larakhaaap@gmail.com</a></li>
-            <li>Telepon: 081325024166 </li>
-        </ul>
-    </section>
-
-    <footer>
-        <p>&copy; 2024 Larakha. Semua hak cipta dilindungi.</p>
-    </footer>
-    <script src="assets/js/login.js"></script>
+  <div class="mt-8">
+    <h2 class="text-lg font-bold">Kontak</h2>
+    <div class="mt-2">
+      <p>Anda bisa menghubungi saya melalui:</p>
+      <p class="mt-1">Email: <a href="mailto:larakhaap@gmail.com" class="underline">larakhaap@gmail.com</a></p>
+      <p class="mt-1">Telepon: <a href="tel:081325024166" class="underline">081325024166</a></p>
+    </div>
+  </div>
+</div>
 </body>
 </html>
